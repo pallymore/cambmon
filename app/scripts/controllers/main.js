e3b7f34dfe703b697501e8551bbd4681fdf479cb
Yurui Zhang <eraclaymore@hotmail.com>
2014-06-14 13:40:14 -0400

    list apps

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,7 +2,7 @@
 
 angular.module('cambMonApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
+    $http.get('/api/apps').success(function(apps) {
+      $scope.apps = apps;
     });
   });
