b2531ef0d5da900c9558554d0c66a582f83a99a4
Yurui Zhang <eraclaymore@hotmail.com>
2014-06-15 18:20:14 -0400

    refactor scope functions into directives

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -18,31 +18,36 @@ angular.module('cambMonApp')
   .controller('DynoCtrl', function ($scope, $http, $routeParams) {
     $http.get('/api/apps/' + $routeParams.name + '/dynos').success(function(dynos) {
       $scope.dynos = dynos;
-
-      $scope.dynoMessage = function (dyno) {
-        switch(dyno.state) {
-        case 'up':
-          return 'System operational.';
-        case 'down':
-          return 'System offline.';
-        case 'idle':
-          return 'No doing anything.';
-        case 'crashed':
-          return 'Aw, Snap!';
-        case 'starting':
-          return 'Starting up...';
-        }
-      };
-
-      $scope.dynoIconClass = function (type) {
-        switch(type) {
-        case 'web':
-          return 'fa-cube';
-        case 'log':
-          return 'fa-file-text-o';
-        case 'worker':
-          return 'fa-cogs';
-        }
-      };
     });
+  })
+  .directive('dynoStatusMessage', function () {
+    var messages = {
+      up: 'System operational.',
+      down: 'System offline.',
+      idle: 'No doing anything.',
+      crashed: 'Aw, Snap!',
+      starting: 'Starting up...',
+      restarting: 'System restarting.'
+    };
+
+    return {
+      template: "{{dynoMessage}}",
+      link: function (scope) {
+        scope.dynoMessage = messages[scope.dyno.state];
+      }
+    };
+  })
+  .directive('dynoTypeIcon', function () {
+    var icons = {
+      web: 'fa-cube',
+      log: 'fa-file-text-o',
+      worker: 'fa-cogs'
+    };
+
+    return {
+      template: "<i class='fa {{dynoIcon}}'></i>",
+      link: function (scope) {
+        scope.dynoIcon = icons[scope.dyno.type];
+      }
+    };
   });
