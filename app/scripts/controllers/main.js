2d180b95ba7f29fa200264f5cae0c7fe02fbb0da
Yurui Zhang <eraclaymore@hotmail.com>
2014-06-14 22:20:50 -0400

    add status display

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -18,5 +18,31 @@ angular.module('cambMonApp')
   .controller('DynoCtrl', function ($scope, $http, $routeParams) {
     $http.get('/api/apps/' + $routeParams.name + '/dynos').success(function(dynos) {
       $scope.dynos = dynos;
+
+      $scope.dynoMessage = function (dyno) {
+        switch(dyno.state) {
+        case 'up':
+          return 'System operational.';
+        case 'down':
+          return 'System offline.';
+        case 'idle':
+          return 'No doing anything.';
+        case 'crashed':
+          return 'Aw, Snap!';
+        case 'starting':
+          return 'Starting up...';
+        }
+      };
+
+      $scope.dynoIconClass = function (type) {
+        switch(type) {
+        case 'web':
+          return 'fa-cube';
+        case 'log':
+          return 'fa-file-text-o';
+        case 'worker':
+          return 'fa-cogs';
+        }
+      };
     });
   });
