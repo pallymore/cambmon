118bb89297e508a055038715c9a19be9135ffe5c
Yurui Zhang <eraclaymore@hotmail.com>
2015-03-02 15:51:22 -0500

    add PX server type support

--- app/scripts/dyno/DynosController.js
+++ app/scripts/dyno/DynosController.js
@@ -8,11 +8,23 @@ angular.module('cambMonApp')
           if (dyno.name === data.source) {
             $scope.$apply(function () {
               dyno.currentRam = parseFloat(data["sample#memory_total"]);
+              dyno.maxRAM = dynoMaxRAM(dyno);
             });
           }
         });
       }
     });
+
+    var dynoMaxRAM = function dynoSize (dyno) {
+      switch (dyno.size) {
+        case "1X":
+          return 512;
+        case "2X":
+          return 1024;
+        case "PX":
+          return 6144;
+      }
+    };
 
     $scope.fetchDynos();
   });
